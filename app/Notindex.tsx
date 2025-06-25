50112cc9adb027dc793ccf89465a8d80a691f409
pedro.lima350 <pedro.lima350@etec.sp.gov.br>
2025-06-25 05:43:03 -0300

    Imc no index

---
+++ app/Notindex.tsx
@@ -0,0 +1,29 @@
+import Input from "@/components/Input";
+import React from "react";
+import { useColorScheme, View } from "react-native";
+
+
+export default function homeScreen(){
+
+    return (
+        <View>aaaaaabbbbba
+        <Input>
+        </Input>
+        </View>
+        
+
+        
+      
+    )
+}
+
+function MyComponent() {
+  let colorScheme = useColorScheme();
+
+  if (colorScheme === 'dark') {
+    // render some dark thing
+  } else {
+    // render some light thing
+  }
+}
+
