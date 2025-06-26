08d471185dae544329580bead1e315075b850d58
Morning_Coffee <blackghost3241@gmail.com>
2025-06-26 18:29:18 -0300

    ajustes

--- app/details/info/index.tsx
+++ app/details/info/index.tsx
@@ -7,7 +7,8 @@ export default function teste(){
     return(
         <View>
             <Text>
-                Estou na stack TESTES
+                Estou na stack
+                
             </Text>
 
         </View>
